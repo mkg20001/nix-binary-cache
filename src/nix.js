7f87e862b8ad78c4a929bf301b19753cb5ec82e2
Maciej Krüger <mkg20001@gmail.com>
2019-12-12 11:19:01 +0100

    fix: pipe

--- src/nix.js
+++ src/nix.js
@@ -11,7 +11,11 @@ const cp = require('child_process')
 const sodium = require('sodium').api
 
 function makeStream (cmd, args, stdin) {
-  const p = cp.spawn(cmd, args, { stdio: [stdin || 'pipe', 'pipe', 'inherit'] })
+  const p = cp.spawn(cmd, args, { stdio: ['pipe', 'pipe', 'inherit'] })
+
+  if (stdin) {
+    stdin.pipe(p.stdin)
+  }
 
   return p.stdout
 }
