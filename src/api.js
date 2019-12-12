999007fe97c3480e431a8b989fb0c641014d1379
Maciej Krüger <mkg20001@gmail.com>
2019-12-12 08:56:59 +0100

    fix: paths

--- src/api.js
+++ src/api.js
@@ -10,7 +10,12 @@ const Boom = require('@hapi/boom')
 
 const NARINFO_RE = /^([0-9a-z]+)\.narinfo$/
 const NAR_BZ2_RE = /^([0-9a-z]+)\.nar\.bz2$/
-const NAR_RE = /^([0-9a-z]+)\.nar$/
+const NAR_RE = /^([0-9a-z]+)\.nar(\.([a-z0-9]+))?$/
+
+const compMap = {
+  xz: 'lzma',
+  bz2: 'pbzip2'
+}
 
 module.exports = async (server, config) => {
   const nix = Nix(config.nix)
@@ -30,61 +35,66 @@ module.exports = async (server, config) => {
 
   server.route({
     method: 'GET',
+    path: '/{hashPart}.nar',
+    handler: async (request, h) => {
+      const hashPart = request.params.hashPart
+
+      const res = await cache.get(req, async () => {
+        const StorePath = await nix.queryPathFromHashPart(hashPart)
+
+        if (!StorePath) { // TODO: maybe also cache 404s?
+          throw Boom.notFound('No such path.')
+        }
+
+        const { narHash, narSize, time, deriver, refs } = await nix.queryPathInfo(StorePath)
+
+        return kvStr({
+          StorePath,
+          URL: `nar/${hashPart}.nar`,
+          Compression: 'none', // TODO: bz2 as standard
+          NarHash: narHash,
+          NarSize: narSize
+          // References:  TODO: add
+          // Deriver: // TODO: add
+          // Sig: // TODO: add
+        })
+      })
+
+      return h.response(res).header('Content-Type', 'text/x-nix-narinfo')
+    }
+  })
+
+  server.route({
+    method: 'GET',
     path: '/nar/{nar}',
     handler: async (request, h) => {
       const req = request.params.nar
 
-      let m
-      let hashPart
-
-      if ((m = req.match(NARINFO_RE))) {
-        hashPart = m[1]
-
-        const res = await cache.get(req, async () => {
-          const StorePath = await nix.queryPathFromHashPart(hashPart)
-
-          if (!StorePath) { // TODO: maybe also cache 404s?
-            throw Boom.notFound('No such path.')
-          }
-
-          const { narHash, narSize, time, deriver, refs } = await nix.queryPathInfo(StorePath)
-
-          return kvStr({
-            StorePath,
-            URL: `nar/${hashPart}.nar`,
-            Compression: 'none', // TODO: bz2 as standard
-            NarHash: narHash,
-            NarSize: narSize
-            // References:  TODO: add
-            // Deriver: // TODO: add
-            // Sig: // TODO: add
-          })
-        })
-
-        return h.response(res).header('Content-Type', 'text/x-nix-narinfo')
-      } else if ((m = req.match(NAR_RE))) {
-        hashPart = m[1]
-
-        const stream = await cache.get(req, async () => {
-          const StorePath = await nix.queryPathFromHashPart(hashPart)
-
-          if (!StorePath) { // TODO: maybe also cache 404s?
-            throw Boom.notFound('No such path.')
-          }
-
-          const stream = nix.dumpStoreStream(StorePath)
-
-          if (false /* compression TODO */) {
-            // TODO
-          }
-
-          return stream
-        })
-
-        return h.response(stream)
-      } else {
+      const m = req.match(NAR_RE)
+      if (!m) {
         throw Boom.notFound('Not a valid nix path.')
       }
+
+      const hashPart = m[1]
+      const comp = m[3]
+
+      const stream = await cache.get(req, async () => {
+        const StorePath = await nix.queryPathFromHashPart(hashPart)
+
+        if (!StorePath) { // TODO: maybe also cache 404s?
+          throw Boom.notFound('No such path.')
+        }
+
+        const tool = compMap[comp]
+
+        if (!comp || tool) {
+          return nix.dumpStoreStream(StorePath, tool)
+        } else {
+          throw Boom.notFound('No such compression.')
+        }
+      })
+
+      return h.response(stream)
     }
   })
 }
